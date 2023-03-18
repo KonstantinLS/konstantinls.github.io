b40138488eea8286c097fc293d9a26af241f7977
Konstantin Lebedev <konstantin.lebedev@one2phone.ru>
2023-03-18 23:16:12 +0200

    preview

--- js/site.js
+++ js/site.js
@@ -2,7 +2,7 @@
 $('div.col').each(function() {
     var galleryImage = $(this).find('img.gallery-item');
     var caption = $(this).find('figcaption').text();
-    var ahchor = jQuery("<a />", {href: galleryImage.attr("src"), "data-caption": caption, "data-toggle": "lightbox", "data-gallery": "gallery"});;
+    var ahchor = jQuery("<a />", {href: galleryImage.attr("src").replace("/preview/", "/"), "data-caption": caption, "data-toggle": "lightbox", "data-gallery": "gallery"});;
 
     galleryImage.wrap(ahchor);
 
